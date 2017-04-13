08f762c10dea3d3c999d939e3f0e10aec806ab95
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-13 09:05:04 -0600

    Add request test for get /api/v1/items

---
+++ spec/requests/items_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Items API" do
+  describe "GET /api/v1/items" do
+    it "returns JSON for all items" do
+      items = Fabricate.times(3, :item)
+
+      get "/api/v1/items"
+
+      expect(response).to be_success
+
+      items_json = JSON.parse(response.body)
+
+      expect(items_json.count).to eq(items.count)
+    end
+  end
+end
